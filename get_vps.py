ceffd426cc77f4bd0d3966199c791a64317c4869
pink10000 <kyletrinh1223@gmail.com>
2025-09-24 00:35:41 +0000

    organization (sorry yejin)

--- get_vps.py
+++
@@ -1,6 +0,0 @@
-from scamper import ScamperCtrl, ScamperFile, ScamperTrace
-
-with ScamperCtrl(mux='/run/ark/mux') as ctrl:
-    vps = [vp for vp in ctrl.vps() if 'network:ipv6' in vp.tags]
-    for vp in vps:
-        print(vp.name)
--- ipv_comparison/traceroute.py
+++ ipv_comparison/traceroute.py
@@ -1,12 +1,16 @@
 import sys
+import os
 from datetime import timedelta
 from scamper import ScamperCtrl, ScamperFile, ScamperTrace, ScamperPing
 import pandas as pd
 import pprint
 
-WARTS_OUT = "foo.warts.gz"
+script_dir = os.path.dirname(os.path.abspath(__file__))
+data_path = os.path.join(script_dir, '..', 'data', 'v4_is_vpn_v6_is_same_org_as_v4_50rows.csv')
+
+WARTS_OUT = "ipv_comparison/foo.warts.gz"
 outfile = ScamperFile(WARTS_OUT, "w")
-df = pd.read_csv('data/v4_is_vpn_v6_is_same_org_as_v4_50rows.csv')
+df = pd.read_csv(data_path)
 
 def get_vps(ctrl):
     base_vps = [
@@ -25,13 +29,11 @@ with ScamperCtrl(mux='/run/ark/mux', outfile=outfile) as ctrl:
         print(f"Instance {inst_index}")
         for ip_index, (ipv4, ipv6) in enumerate(zip(df['IPv4'], df['IPv6'])):
             ipv4, ipv6 = str(ipv4), str(ipv6)
-            id = inst_index * 10 + ip_index
-            print(f"{id:4} {ipv4} {ipv6}")
+            id = inst_index * df.shape[0] + ip_index
+            print(f"{id:4} {ipv4:15} {ipv6:30}")
             ctrl.do_trace(ipv4, inst = ctrl_inst, method='icmp-paris', attempts=1, wait_timeout=timedelta(seconds=1), userid = id)
             ctrl.do_trace(ipv6, inst= ctrl_inst, method='icmp-paris', attempts=1, wait_timeout=timedelta(seconds=1), userid = id)
 
     for obj in ctrl.responses(timeout=timedelta(seconds=100)):
         if not isinstance(obj, ScamperTrace):
             continue
-
-
