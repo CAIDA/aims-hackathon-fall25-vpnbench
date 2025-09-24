571dc30eeba99327e02ca67d4e85ad710ab32952
pink10000 <kyletrinh1223@gmail.com>
2025-09-24 00:25:09 +0000

    use geographical distant vps

--- traceroute.py
+++ traceroute.py
@@ -1,23 +1,32 @@
 import sys
 from datetime import timedelta
-from scamper import ScamperCtrl, ScamperFile, ScamperTrace
+from scamper import ScamperCtrl, ScamperFile, ScamperTrace, ScamperPing
 import pandas as pd
 import pprint
 
 WARTS_OUT = "foo.warts.gz"
 outfile = ScamperFile(WARTS_OUT, "w")
-df = pd.read_csv('data/v4_is_vpn_v6_is_same_org_as_v4_50rows.csv')[:10]
+df = pd.read_csv('data/v4_is_vpn_v6_is_same_org_as_v4_50rows.csv')
+
+def get_vps(ctrl):
+    base_vps = [
+        "cld4-us.ark.caida.org",
+        "waw-pl.ark.caida.org",
+        "dmk2-th.ark.caida.org",
+    ]
+    return [vp for vp in ctrl.vps() if vp.name in base_vps]
 
 with ScamperCtrl(mux='/run/ark/mux', outfile=outfile) as ctrl:
-    # print(vps, end='\n')
-    vps = [vp for vp in ctrl.vps() if 'network:ipv6' in vp.tags][:3]
-    ctrl.add_vps(vps)
+    # if for some reason these are down, we have bigger problems
+
+    ctrl.add_vps(get_vps(ctrl))
 
     for inst_index, ctrl_inst in enumerate(ctrl.instances()):
+        print(f"Instance {inst_index}")
         for ip_index, (ipv4, ipv6) in enumerate(zip(df['IPv4'], df['IPv6'])):
             ipv4, ipv6 = str(ipv4), str(ipv6)
             id = inst_index * 10 + ip_index
-            print(id, ipv4, ipv6)
+            print(f"{id:4} {ipv4} {ipv6}")
             ctrl.do_trace(ipv4, inst = ctrl_inst, method='icmp-paris', attempts=1, wait_timeout=timedelta(seconds=1), userid = id)
             ctrl.do_trace(ipv6, inst= ctrl_inst, method='icmp-paris', attempts=1, wait_timeout=timedelta(seconds=1), userid = id)
 
